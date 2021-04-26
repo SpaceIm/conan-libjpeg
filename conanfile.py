83a69e9b1ba9fc86cf9bab947cacd09679d222cf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-27 00:22:33 +0200

    Revert "try another approach for mingw"
    This reverts commit 84dd0be09ac277970bc47b9544da99bc8effe273.

--- conanfile.py
+++ conanfile.py
@@ -33,11 +33,9 @@ class LibjpegConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def build_requirements(self):
-        if tools.os_info.is_windows and self.settings.compiler != "Visual Studio":
-            if not tools.get_env("CONAN_BASH_PATH") and tools.os_info.detect_windows_subsystem() != "msys2":
-                self.build_requires("msys2/20200517")
-            if not tools.which("make"):
-                self.build_requires("make/4.2.1")
+        if tools.os_info.is_windows and self.settings.compiler != "Visual Studio" and \
+           not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/20200517")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
